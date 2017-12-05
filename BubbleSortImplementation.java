193b2aaea67c1391729129858cfe28879ce544c7
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-12-05 08:00:49 +0000

    Minor Change

--- BubbleSortImplementation.java
+++ BubbleSortImplementation.java
@@ -9,7 +9,7 @@ class BubbleSortImplementation {
 
   public static void main(String[] args) {
     int []numbers = {7,4,6,10,2,5,3};
-    //expected result = 4,6,2,10,3,5
+    //expected result = 2,3,4,5,6,7,10
     bubbleSort(numbers);
 
     print(Arrays.toString(numbers));
