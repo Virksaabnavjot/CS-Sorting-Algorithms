f50e070feb4ff9dfa667b7a48d3385f75487e450
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-12-05 07:58:09 +0000

    Bubble Sort implementation

---
+++ BubbleSortImplementation.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class BubbleSortImplementation {
+
+  public static void print(String output){
+    System.out.println(output);
+  }//print method
+
+  public static void main(String[] args) {
+    int []numbers = {7,4,6,10,2,5,3};
+    //expected result = 4,6,2,10,3,5
+    bubbleSort(numbers);
+
+    print(Arrays.toString(numbers));
+  }//main method
+
+  public static void bubbleSort(int []numbers){
+
+    int n;
+    n = numbers.length-2;
+
+    Boolean swapped = true;
+
+    while(swapped){
+      swapped = false;
+      int temp = 0;
+      for(int i=0; i <= n; i++){
+        if(numbers[i] > numbers[i+1]){
+          temp = numbers[i];
+          numbers[i] = numbers[i+1];
+          numbers[i+1] = temp;
+
+          swapped = true;
+
+        }//if statement
+      }//for loop
+
+    }//while loop
+
+  }//bubble sort
+}//the end
