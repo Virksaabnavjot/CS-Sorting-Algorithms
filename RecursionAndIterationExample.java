1182b5a722e86a04623902cea166c59346160a49
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-12-05 09:47:39 +0000

    Iteration Example - using for loop

--- RecursionAndIterationExample.java
+++ RecursionAndIterationExample.java
@@ -5,14 +5,27 @@ public class RecursionAndIterationExample{
 
   public static void main(String[] args) {
 
+    System.out.println("Answer: "+iterativeFactorialUsingForLoop(6));
+
   }//main method
 
   public static void recursiveFactorial(int number){
 
   }//recursive method/function
 
-  public static void iterativeFactorial(int number){
+  public static int iterativeFactorialUsingForLoop(int number){
+    int factorial = 1; //Since, 0! = 1
 
+    System.out.println("Factorial of: "+number+ "!");
+      int stepCount = 0;
+      for(int i = number; i > 0; i--){
+        factorial = factorial * i;
+        stepCount++;
+        System.out.println("Step: "+(stepCount)+" ---> "+factorial+ " = " +factorial+ " * " +(i-1)+"!");
+      }//for loop
+
+
+   return factorial;
   }//iterative method
 
 }//end of class
